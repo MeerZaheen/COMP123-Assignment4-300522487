986298a16854869a62ee7f16281ec5c9a3b80cb5
meerzaheen_90 <meerzaheen_90@hotmail.com>
2016-07-22 21:03:53 -0400

    Final Submission

---
+++ Airline/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Airline;
+/* 
+ * Author: Meer Zaheen Nazmul
+ * Student ID: 300522487
+ * 
+ * Date: July 17,2016
+ * Description: This program assigns seats on each flight for the Airline's only plane.
+ *  
+ * Version: 0.0.1
+ */
+
+namespace Airline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Private Menu Method+++++++++++++++++++++++++++++++++++++++++++++++++++++
+            /**
+             * The Main method for our driver class program
+             * 
+             * @constructor:Main
+             * @param {string[]}args
+             */
+            int selection = 0;
+            while (selection != 3)
+            {
+                Console.WriteLine("+++++++++++++++++++++++++++++++");
+                Console.WriteLine("+ Airline_Reservations_System +");
+                Console.WriteLine("+                             +");
+                Console.WriteLine("+      1. First Class         +");
+                Console.WriteLine("+      2. Economy Class       +");
+                Console.WriteLine("+      3. Exit                +");
+                Console.WriteLine("+                             +");
+                Console.WriteLine("+++++++++++++++++++++++++++++++");
+                Console.Write("Please make your selection: ");
+
+                try
+                {
+                    selection = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    selection = 0;
+                    Console.WriteLine(error.Message);
+                }
+
+                switch (selection)
+                {
+                    case 1:
+                        ConditionalFirstClass();
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        EconomyClass();
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        Console.WriteLine();
+                        break;
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Incorrect entry - Please try again");
+                        Console.WriteLine();
+                        WaitForKey();
+                        break;
+                }
+            }
+        }
+        // PRIVATE INSTANCE+++++++++++++++++++++++++++++++++++++++++
+        List<String> _seatGuru = new List<string>();
+        List<String> seatSelected = new List<string>();
+
+        // PUBLIC INSTANCE+++++++++++++++++++++++++++++++++++++++++
+        public static void AirlineSystem()
+        {
+            List<String> seatGuru = new List<string>();
+            List<String> seatSelected = new List<string>();
+            // list of First and Enonomy Seatings options
+            seatGuru.Add("First Class Seat 1");
+            seatGuru.Add("First Class Seat 2");
+            seatGuru.Add("First Class Seat 3");
+            seatGuru.Add("First Class Seat 4");
+            seatGuru.Add("First Class Seat 5");
+            seatGuru.Add("Economy Class Seat 6");
+            seatGuru.Add("Economy Class Seat 7");
+            seatGuru.Add("Economy Class Seat 8");
+            seatGuru.Add("Economy Class Seat 9");
+            seatGuru.Add("Economy Class Seat 10");
+        }
+        // PUBLIC METHODS +++++++++++++++++++++
+
+        public void ConditionalFirstClass()
+        {
+            int seat = 0;
+            while (seat < 5)
+            {
+                if (seatGuru[seat] == 0)
+                {
+                    seatSelected.Add(seatGuru[seat]);
+                    seatGuru.RemoveAt(seat);
+                    this.print();
+                    break;
+                }
+                else {
+                    Console.WriteLine("First class is fully booked");
+                }
+                seat++;
+            }
+        }
+        public void print()
+        {
+            Console.WriteLine("Congratulations! Your Seat is Booked");
+        }
+    }
+ }
+
---
+++ DiceSim/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 
+ * Author: Meer Zaheen Nazmul
+ * Student ID: 300522487
+ * 
+ * Date: July 17,2016
+ * Description: This program simulates the rolling of two dice and displaying how many times
+ * each result was rolled.
+ *  
+ * Version: 0.0.1
+ */
+namespace DiceSim
+{
+    /**
+     * The Maim method for our driver class program
+     * @class: program
+     */
+    class Program
+    {
+        /**
+         * The Main method for our driver class program
+         * 
+         * @constructor:Main
+         * @param {string[]}args
+         */
+        static void Main(string[] args)
+        {
+            //instance variables+++++++++++++++++++++++++++++++
+
+            ///Object Of Class Random
+            Random random = new Random();
+            ///First die
+            int firstDie = 0;
+            ///Second die
+            int secondDie = 0;
+            ///Sum of FirstDie and SecondDie
+            int sum;
+            ///New List 
+            List<int> Rolls = new List<int>();
+            ///# of times the app should roll
+            int rollCount = 36000;
+            int[] tallyDimension = new int[36000];
+
+            for (int index = 0; index < rollCount; index++)
+            {
+                firstDie = random.Next(1, 7);  //Random Rolls from 1 - 6
+                secondDie = random.Next(1, 7); //Random Rolls from 1 - 6
+                ///Sum of the two values
+                sum = firstDie + secondDie;
+                ///the number of times each possible sum appears               
+                tallyDimension[sum]++;
+            }
+            Console.WriteLine("Roll #  | # of times rolled | ");
+            Console.WriteLine("-----------------------------");
+            for (int index = 2; index < 13; index++)
+            {
+
+                Console.WriteLine("{0, 5}   | {1, 10}        |", index, tallyDimension[index]);
+            }
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine();
+
+        }
+
+    }
+}
